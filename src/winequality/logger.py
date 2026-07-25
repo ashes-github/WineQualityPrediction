0f62ab3bcf905c96bf075a0c8c1a020e8825505a
ashes-github <ashesrc@gmail.com>
2026-07-25 18:56:41 +0530

    Modified logger for having single log file for all the pipeline stages

--- src/winequality/logger.py
+++ src/winequality/logger.py
@@ -1,19 +1,26 @@
 import logging
 import os
 import sys
-from datetime import datetime
+from logging.handlers import RotatingFileHandler
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE = "pipeline.log"
 logs_path = os.path.join(os.getcwd(), "logs")
 os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
+file_handler = RotatingFileHandler(
+    LOG_FILE_PATH,
+    maxBytes=5 * 1024 * 1024,
+    backupCount=3,
+    encoding="utf-8",
+)
+
 logging.basicConfig(
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
     handlers=[
-        logging.FileHandler(LOG_FILE_PATH),
+        file_handler,
         logging.StreamHandler(sys.stdout),
     ],
 )
